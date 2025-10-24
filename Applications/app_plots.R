e95f70c148884ae997464da0f5cfbf91eb6c1b66
Lingxiao Zhou <zhou.l@ufl.edu>
2025-10-23 22:25:39 -0400

    add analysis for adaptive intervnetions

--- Applications/app_plots.R
+++ Applications/app_plots.R
@@ -322,4 +322,189 @@ for (al in 1:length(aid_lag)) {
   
 }
 
+#---------------------------Figure A.23-------------------
+
+truncation_vec <- c(0.95)
+for (truncation in truncation_vec) {
+  for (al in 1:length(aid_lag)) {
+    save_path <- paste0(outpath,aid_lag_name[al],"/")
+    
+    # make df for plot
+    df_plot <- data.frame(array(0,c(0,7)))
+    colnames(df_plot) <- c("x","m","ub","lb","M","Outcome","Meaneff")
+    for (oo in c("SAF","IED")) {
+      for (mm in 1:10) {
+        scale <- 81
+        load(paste0(save_path, "res_Type1_M", mm,"_",oo,"_trun_aid_bi_",truncation*100,".dat"))
+        
+        m <- res$est_eval *scale
+        V <- diag(res$V_eval)*scale^2
+        x <- res$specification$eval_values
+        ub <- m+qnorm(c(0.975))*sqrt(V)
+        lb <- m+qnorm(c(0.025))*sqrt(V)
+        meaneffect <- res$mean_effect*scale
+        
+        
+        df_tmp <- cbind.data.frame(x = x,m,ub,lb,M = mm,Outcome = oo,Meaneff = meaneffect)
+        df_plot <- rbind.data.frame(df_plot,df_tmp)
+        
+        
+      }
+      
+    }
+    
+    
+    df_plot$Aid <- as.factor(df_plot$x)
+    levels(df_plot$Aid) <- c("Without aid", "With aid")
+    df_plot$M <- as.factor(df_plot$M)
+    levels(df_plot$M) <-  paste0("M=",1:10)
+    
+    p <- ggplot(df_plot, aes(x = M, y = m,color = Aid)) +
+      geom_point(position = position_dodge(width = 0.5)) +
+      geom_errorbar(aes(ymin = lb, ymax = ub), width = 0.2, position = position_dodge(width = 0.5)) +
+      geom_hline(yintercept = 0, linetype = "dashed", color = "black")+
+      facet_grid(Outcome ~ ., scales = "free_y") +
+      labs(x = "", y = "CATE (District Effect)") +
+      theme_bw()+
+      theme(
+        axis.text.x = element_text(size = 12),  # Increase size of x-axis tick labels
+        axis.text.y = element_text(size = 12)   # Increase size of y-axis tick labels
+      )+
+      theme(legend.position = "top")+
+      scale_color_manual(values = c("With aid" = "#2C7A7B", "Without aid" = "#F4A261"))
+    print(p)
+    
+    if(save_plot){
+      ggsave(file = paste0(save_path,"plot_binary_cate_",aid_lag_name[al],"_",truncation*100,".pdf"), plot = p, width = 6, height = 3.5, units = "in", device = "pdf")
+    }
+    
+  }
+}
+
+#---------------------------Figure A.24-------------------
+
+for (truncation in truncation_vec) {
+  for (al in 1:length(aid_lag)) {
+    save_path <- paste0(outpath,aid_lag_name[al],"/")
+    
+    # make df for plot
+    df_plot <- data.frame(array(0,c(0,5)))
+    colnames(df_plot) <- c("m","ub","lb","M","Outcome")
+    for (oo in c("SAF","IED")) {
+      for (mm in 1:10) {
+        scale <- 81
+        load(paste0(save_path, "res_Type1_M", mm,"_",oo,"_trun_aid_bi_",truncation*100,".dat"))
+        m <- res$est_beta[2]*scale
+        V <- diag(res$V_beta)[2]*scale^2
+        
+        ub <- m+qnorm(c(0.975))*sqrt(V)
+        lb <- m+qnorm(c(0.025))*sqrt(V)
+        df_tmp <- cbind.data.frame(m,ub,lb,M = mm,Outcome = oo)
+        
+        
+        
+        df_plot <- rbind.data.frame(df_plot,df_tmp)
+        
+        
+      }
+      
+    }
+    
+    
+    
+    df_plot$M <- as.factor(df_plot$M)
+    levels(df_plot$M) <-  paste0("M=",1:10)
+    
+    p <- ggplot(df_plot, aes(x = M, y = m)) +
+      geom_point(position = position_dodge(width = 0.5)) +
+      geom_errorbar(aes(ymin = lb, ymax = ub), width = 0.2, position = position_dodge(width = 0.5)) +
+      geom_hline(yintercept = 0, linetype = "dashed", color = "black")+
+      facet_grid(Outcome ~ ., scales = "free_y") +
+      labs(x = "", y = "Expected difference") +
+      theme_bw()+
+      theme(
+        axis.text.x = element_text(size = 12),  # Increase size of x-axis tick labels
+        axis.text.y = element_text(size = 12)   # Increase size of y-axis tick labels
+      )+
+      theme(legend.position = "bottom")
+    
+    print(p)
+    if(save_plot){
+      ggsave(file = paste0(save_path,"plot_binary_beta_",aid_lag_name[al],"_",truncation*100,".pdf"), plot = p, width = 6, height = 3, units = "in", device = "pdf")
+    }
+    
+  }
+  
+}
+
+#---------------------------Figure A.25-------------------
+
+for (truncation in truncation_vec) {
+  for (al in 1:length(aid_lag)) {
+    save_path <- paste0(outpath,aid_lag_name[al],"/")
+    
+    # make df for plot
+    df_plot <- data.frame(array(0,c(0,8)))
+    colnames(df_plot) <- c("x","m","ub","lb","M","Outcome","Meaneff","pvalue")
+    for (oo in c("SAF","IED")) {
+      for (mm in c(3,7,10)) {
+        scale <- 81
+        load(paste0(save_path, "res_Type1_M", mm,"_",oo,"_trun_aid_percapita_",truncation*100,".dat"))
+        m <- res$est_eval *scale
+        V <- diag(res$V_eval)*scale^2
+        x <- res$specification$eval_values
+        ub <- m+qnorm(c(0.975))*sqrt(V)
+        lb <- m+qnorm(c(0.025))*sqrt(V)
+        meaneffect <- res$mean_effect*scale
+        df_tmp <- cbind.data.frame(x = x,m,ub,lb,M = mm,Outcome = oo,Meaneff = meaneffect,pvalue = res$p.value)
+        df_plot <- rbind.data.frame(df_plot,df_tmp)
+        
+        
+      }
+      
+    }
+    
+    Mlist <- c(3,7,10)
+    
+    df_plot <- df_plot[df_plot$M%in%Mlist,]
+    df_plot$M <- as.factor(df_plot$M)
+    df_plot$x <- exp(df_plot$x)-1
+    levels(df_plot$M) <-  paste0("M=",Mlist)
+    
+    a <- df_plot[,c(5,6,8)]
+    a <- a %>% distinct()
+    
+    p <- ggplot() +
+      geom_point(data = subset(df_plot, df_plot$x==0), aes(x = x, y = m), color = "black") +
+      geom_errorbar(data = subset(df_plot, df_plot$x==0), aes(x = x, ymin = lb, ymax = ub), width = 0.1) +
+      geom_line(data = subset(df_plot, df_plot$x!=0), aes(x = x, y = m), color = "black") +
+      geom_hline(yintercept = 0, linetype = "dashed", color = "black")+
+      geom_hline(data = df_plot, aes(yintercept = Meaneff), linetype = "dashed", color = "#D55E00")+
+      geom_ribbon(data = subset(df_plot, df_plot$x!=0), aes(x = x, ymin = lb, ymax = ub), fill = "grey", alpha = 0.5) +
+      facet_grid(Outcome~M, scales = "free_y") +theme_bw()+
+      labs(x = "Aid per capita", y = "CATE (District Effect)")+
+      theme(
+        axis.text.x = element_text(size = 12),  # Increase size of x-axis tick labels
+        axis.text.y = element_text(size = 12)   # Increase size of y-axis tick labels
+      ) + geom_text(
+        size    = 4.5,
+        data    = a,
+        mapping = aes(x = Inf, y = Inf, label = sprintf("p-value=%.3f", pvalue)),
+        hjust   = 1.05,
+        vjust   = 10.8
+      )
+    
+    
+    
+    
+    
+    print(p)
+    
+    if(save_plot){
+      ggsave(file = paste0(save_path,"plot_continuous_",aid_lag_name[al],"_",truncation*100,".pdf"), plot = p, width = 7, height = 3.8, units = "in",device = "pdf")
+    }
+    
+    
+  }
+}
 
--- Applications/application_code.R
+++ Applications/application_code.R
@@ -2,14 +2,16 @@ library(geocausal)
 library(splines)
 library(spatstat)
 library(tidyverse)
+library(dplyr)
 
 outpath <- "~/GitHub/CATE-SpatioTemporal-Causal/Applications/Results/"
 save_res <- FALSE
 
 airstr <- airstrikes
 insurg <- insurgencies
-airstr_2006 <- airstrikes_base
-
+airstr_2006 <- airstrikes_2006 %>%
+  filter(type == "Airstrike") %>%   # keep only rows where type == "Airstrike"
+  select(-type)                     # then drop the column 'type'
 
 airstr$time <- as.numeric(airstr$date - min(airstr$date) + 1)
 insurg$time <- as.numeric(insurg$date - min(insurg$date) + 1)
@@ -463,7 +465,308 @@ for (al in 1:length(aid_lag)) {
 }
 
 
+#------------------------Adaptive intervention-----------------------------------
 
+outpath <- "~/GitHub/CATE-SpatioTemporal-Causal/Applications/Results/adaptive/"
+
+
+airstr2006 <- airstrikes_2006
+airstr2006$time <- as.numeric(airstr2006$date - min(airstr2006$date) + 1)
+insurg2006 <- insurgencies_2006
+insurg2006$time <- as.numeric(insurg2006$date - min(insurg2006$date) + 1)
+
+
+
+
+
+treatment_hfr_baseline <- get_hfr(data = airstr2006, col = "type", window = iraq_window,
+                                  time_col = "time", time_range = c(1, 276),
+                                  coordinates = c("longitude", "latitude"),
+                                  combine = TRUE)
+outcome_hfr_baseline <- get_hfr(data = insurg2006, col = "type", window = iraq_window,
+                                time_col = "time", time_range = c(1, 276),
+                                coordinates = c("longitude", "latitude"),
+                                combine = TRUE)
+
+
+dat_hfr_baseline <- spatstat.geom::cbind.hyperframe(treatment_hfr_baseline, outcome_hfr_baseline[, -1])
+names(dat_hfr_baseline)[names(dat_hfr_baseline) == "all_combined"] <- "all_treatment"
+names(dat_hfr_baseline)[names(dat_hfr_baseline) == "all_combined.1"] <- "all_outcome"
+
+
+#-------------------------transform covariates-----------------
+
+# cities
+cities_dist <- dataverse::get_dataframe_by_name(
+  filename = "cities_dist.rds", dataset = "doi:10.7910/DVN/IU8RQK",
+  server = "dataverse.harvard.edu", .f = readRDS, original = TRUE)
+cities_dist <- lapply(1:5, function(x) exp(-(2 * x) * cities_dist$distance[[x]]))
+dat_hfr_baseline$all_cities <- cities_dist[[1]] + cities_dist[[2]] + 
+  cities_dist[[3]] + cities_dist[[4]] + cities_dist[[5]]
+
+# rivers
+rivers_dist <- dataverse::get_dataframe_by_name(
+  filename = "rivers_dist.rds", dataset = "doi:10.7910/DVN/IU8RQK",
+  server = "dataverse.harvard.edu", .f = readRDS, original = TRUE)
+dat_hfr_baseline$rivers_dist <- exp(-3 * rivers_dist)
+
+# routes
+routes_dist <- dataverse::get_dataframe_by_name(
+  filename = "routes_dist.rds", dataset = "doi:10.7910/DVN/IU8RQK",
+  server = "dataverse.harvard.edu", .f = readRDS, original = TRUE)
+dat_hfr_baseline$routes_dist <- exp(-3 * routes_dist)
+
+# settles
+settle_dist <- dataverse::get_dataframe_by_name(
+  filename = "settle_dist.rds", dataset = "doi:10.7910/DVN/IU8RQK",
+  server = "dataverse.harvard.edu", .f = readRDS, original = TRUE)
+for (jj in 1 : 18) {
+  dat_hfr_baseline$settles <- exp(-12 * settle_dist$distance[[jj]])
+  col_name <- paste0("settles_dist_", jj)
+  names(dat_hfr_baseline)[length(names(dat_hfr_baseline))]<- col_name
+}
+
+
+#---------------------get treatment and outcome histories--------
+
+get_out_hx <- function(timeint, out, window, lag) {
+  temp <- lapply(timeint, get_hist, Xt = out, lag = lag, window = window)
+  purrr::map(temp, 1)
+}
+
+
+dat_hfr_baseline$hist_allout_1 <- get_out_hx(timeint = dat_hfr_baseline$time, 
+                                             out = dat_hfr_baseline$all_outcome,
+                                             window = iraq_window, lag = 1)
+dat_hfr_baseline$hist_allout_7 <- get_out_hx(timeint = dat_hfr_baseline$time, 
+                                             out = dat_hfr_baseline$all_outcome,
+                                             window = iraq_window, lag = 7)
+dat_hfr_baseline$hist_allout_30 <- get_out_hx(timeint = dat_hfr_baseline$time, 
+                                              out = dat_hfr_baseline$all_outcome,
+                                              window = iraq_window, lag = 30)
+
+
+
+dat_hfr_baseline$hist_alltre_1 <- get_out_hx(timeint = dat_hfr_baseline$time, 
+                                             out = dat_hfr_baseline$Airstrike,
+                                             window = iraq_window, lag = 1)
+dat_hfr_baseline$hist_alltre_7 <- get_out_hx(timeint = dat_hfr_baseline$time, 
+                                             out = dat_hfr_baseline$Airstrike,
+                                             window = iraq_window, lag = 7)
+dat_hfr_baseline$hist_alltre_30 <- get_out_hx(timeint = dat_hfr_baseline$time, 
+                                              out = dat_hfr_baseline$Airstrike,
+                                              window = iraq_window, lag = 30)
+
+
+
+dat_hfr_baseline$hist_sof_1 <- get_out_hx(timeint = dat_hfr_baseline$time, 
+                                          out = dat_hfr_baseline$SOF,
+                                          window = iraq_window, lag = 1)
+dat_hfr_baseline$hist_sof_7 <- get_out_hx(timeint = dat_hfr_baseline$time, 
+                                          out = dat_hfr_baseline$SOF,
+                                          window = iraq_window, lag = 7)
+dat_hfr_baseline$hist_sof_30 <- get_out_hx(timeint = dat_hfr_baseline$time, 
+                                           out = dat_hfr_baseline$SOF,
+                                           window = iraq_window, lag = 30)
+
+
+# help function for transforming the point pattern to distance to the point pattern
+get_prior_hist <- function(x, prior_coef, window) {
+  
+  dist_mp <- spatstat.geom::distmap(x)
+  dims <- dim(dist_mp$v)
+  if (length(unique(as.numeric(dist_mp$v))) == 1) {
+    dist_mp$v <- matrix(Inf, nrow = dims[1], ncol = dims[2])
+  }
+  prior_hx <- exp(- prior_coef * dist_mp)
+  spatstat.geom::Window(prior_hx) <- window
+  
+  return(list(prior_hx = prior_hx))
+  
+}
+
+dat_hfr_baseline$hist_allout_1 <- purrr::map(lapply(dat_hfr_baseline$hist_allout_1, get_prior_hist, 
+                                                    prior_coef = 6, window = iraq_window), 1)
+dat_hfr_baseline$hist_allout_7 <- purrr::map(lapply(dat_hfr_baseline$hist_allout_7, get_prior_hist, 
+                                                    prior_coef = 6, window = iraq_window), 1)
+dat_hfr_baseline$hist_allout_30 <- purrr::map(lapply(dat_hfr_baseline$hist_allout_30, get_prior_hist, 
+                                                     prior_coef = 6, window = iraq_window), 1)
+
+
+
+dat_hfr_baseline$hist_alltre_1 <- purrr::map(lapply(dat_hfr_baseline$hist_alltre_1, get_prior_hist, 
+                                                    prior_coef = 6, window = iraq_window), 1)
+dat_hfr_baseline$hist_alltre_7 <- purrr::map(lapply(dat_hfr_baseline$hist_alltre_7, get_prior_hist, 
+                                                    prior_coef = 6, window = iraq_window), 1)
+dat_hfr_baseline$hist_alltre_30 <- purrr::map(lapply(dat_hfr_baseline$hist_alltre_30, get_prior_hist, 
+                                                     prior_coef = 6, window = iraq_window), 1)
+
+
+
+dat_hfr_baseline$hist_sof_1 <- purrr::map(lapply(dat_hfr_baseline$hist_sof_1, get_prior_hist, 
+                                                 prior_coef = 6, window = iraq_window), 1)
+dat_hfr_baseline$hist_sof_7 <- purrr::map(lapply(dat_hfr_baseline$hist_sof_7, get_prior_hist, 
+                                                 prior_coef = 6, window = iraq_window), 1)
+dat_hfr_baseline$hist_sof_30 <- purrr::map(lapply(dat_hfr_baseline$hist_sof_30, get_prior_hist, 
+                                                  prior_coef = 6, window = iraq_window), 1)
+# get ethnicity
+ethnicity <- dataverse::get_dataframe_by_name(
+  filename = "ethnicity.rds", dataset = "doi:10.7910/DVN/IU8RQK",
+  server = "dataverse.harvard.edu", .f = readRDS, original = TRUE)
+
+
+# get log population
+dat_hfr_baseline$pop <- log(ethnicity$total_pop)
+
+
+
+rhs_var_baseline <- c("pop", "rivers_dist", "routes_dist", "all_cities",
+                      "hist_allout_1", "hist_allout_7", "hist_allout_30",
+                      "hist_sof_1", "hist_sof_7", "hist_sof_30",
+                      "settles_dist_1", "settles_dist_2", "settles_dist_3", 
+                      "settles_dist_4", "settles_dist_5", "settles_dist_6", 
+                      "settles_dist_7", "settles_dist_8", "settles_dist_9", 
+                      "settles_dist_10", "settles_dist_11", "settles_dist_12", 
+                      "settles_dist_13", "settles_dist_14", "settles_dist_15",
+                      "settles_dist_16", "settles_dist_17", "settles_dist_18")
+
+
+baseline_den <- get_adaptive_baseline_dens(dat_hfr_baseline[c(31:nrow(dat_hfr_baseline)), ],dat_hfr[c(31:nrow(dat_hfr)), ],"Airstrike",
+                                           rhs_var_baseline, ngrid = 100, window = iraq_window)
+
+
+
+
+cf_1 <- get_cf_dens_adaptive(baseline_den, scale_factor=1/2)
+cf_2 <- get_cf_dens_adaptive(baseline_den, scale_factor=2)
+
+aid_lag <- 1
+aid_lag_name <- "onemonth"
+
+#-------------------Binary aid analysis------------------------------
+truncation_vec <- c(0.95)
+
+for (truncation in truncation_vec) {
+  for (al in 1:length(aid_lag)) {
+    cat("Start al: ",al, "\n")
+    
+    
+    save_path <- paste0(outpath,aid_lag_name[al],"/")
+    
+    
+    for (oo in c("SAF","IED")) {
+      for (mm in 1:10) {
+        
+        
+        em <- get_em_vec(dat_hfr$aid[31:499],time_after = TRUE,lag=mm,entire_window = NULL,ngrid = c(128,128))
+        em <- em>0
+        E.mat <- matrix(em,ncol = 1)
+        
+        
+        if(oo=="IED"){
+          pixel_count_out <- dat_hfr$pixel_count_IED[31:499]
+        }else{
+          pixel_count_out <- dat_hfr$pixel_count_SAF[31:499]
+        }
+        
+        points <- matrix(c(0,1),ncol=1)
+        cat("Start M =",mm,"Outcome: ",oo,"\n")
+        res <- get_cate(obs=obs_density, cf1 = cf_1, cf2 = cf_2, treat = dat_hfr$Airstrike[31:499], pixel_count_out=pixel_count_out,lag=mm, trunc_level=truncation, time_after=TRUE,entire_window = iraq_window,
+                        E_list = dat_hfr$aid[31:499],E_mat = E.mat,
+                        nbase = 6, spline_type = "ns",intercept = TRUE,
+                        eval_values = points, eval_mat = points)
+        
+        if(save_res){
+          save(res, file = paste0(save_path, "res_Type1_M", mm,"_",oo,"_trun_aid_bi_",truncation*100,".dat"))
+        }
+        
+      }
+    }
+    
+    
+  }
+}
+
+
+
+
+
+#-------------------------Continuous aid--------------------------
+nbaseE <-5
+firsttime <- TRUE
+
+for (truncation in truncation_vec) {
+  for (al in 1:length(aid_lag)) {
+    cat("Start al: ",al, "\n")
+    
+    if(firsttime){
+      get_aid_percapita <- function(t,aid,pop,dimyx=c(128,128)){
+        res <- log(as.im(exp(aid[[t]])-1,dimyx = dimyx)/as.im(exp(pop[[t]])-1,dimyx=dimyx)+1)
+        return(res)
+      }
+      dat_hfr$aid_per_cap <- lapply(dat_hfr$time, function(x) get_aid_percapita(x,aid = dat_hfr$aid, pop=dat_hfr$pop))
+    }
+    
+    
+    save_path <- paste0(outpath,aid_lag_name[al],"/")
+    
+    
+    for (oo in c("SAF","IED")) {
+      for (mm in c(3,7,10)) {
+        
+        
+        em <- get_em_vec(dat_hfr$aid_per_cap[31:499],time_after = TRUE,lag=mm,entire_window = NULL,ngrid = c(128,128))
+        E.mat <- array(em,c(128,128,length(31:(498-mm+1))))
+        
+        myindex <- E.mat>0 & !is.na(E.mat)
+        myindex2 <- E.mat==0 & !is.na(E.mat)
+        
+        # choose the knots
+        e_min <- min(E.mat[myindex],na.rm = TRUE)
+        
+        e_max <- max(E.mat[myindex],na.rm = TRUE)
+        
+        
+        my_knots <- e_min+1:(nbaseE-2)*(e_max-e_min)/(nbaseE-1)
+        
+        E.basis <- array(NA, c(prod(dim(E.mat)),nbaseE-1))
+        E.basis.tmp <- ns(c(E.mat[myindex]),knots = my_knots)
+        
+        E.basis[myindex] <- E.basis.tmp
+        E.basis[myindex2] <- 0
+        E.basis <- cbind(E.basis,c(E.mat==0))
+        
+        tmp_points <- quantile(E.mat[myindex],c(0.1,0.9),na.rm = TRUE)
+        points <- seq(tmp_points[1],tmp_points[2],length.out = 20)
+        points.basis <- cbind(1,predict(E.basis.tmp,newx = points))
+        points <- c(0,points)
+        points.basis <- rbind(0,points.basis)
+        points.basis <- cbind(points.basis,c(1,rep(0,20)))
+        points.basis[1,1] <- 1
+        points.basis <- points.basis[,-1]
+        
+        if(oo=="IED"){
+          pixel_count_out <- dat_hfr$pixel_count_IED[31:499]
+        }else{
+          pixel_count_out <- dat_hfr$pixel_count_SAF[31:499]
+        }
+        
+        
+        cat("Start M=",mm,"Outcome: ",oo,"\n")
+        res <- get_cate(obs=obs_density, cf1 = cf_1, cf2 = cf_2, treat = dat_hfr$Airstrike[31:499], pixel_count_out=pixel_count_out,lag=mm, trunc_level=truncation, time_after=TRUE,entire_window = iraq_window,
+                        E_list = dat_hfr$aid[31:499],E_mat = E.basis,
+                        nbase = 6, spline_type = "ns",intercept = TRUE,
+                        eval_values = points, eval_mat = points.basis,bound = 3,M=mm)
+        if(save_res){
+          save(res, file = paste0(save_path, "res_Type1_M", mm,"_",oo,"_trun_aid_percapita_",truncation*100,".dat"))
+        }
+        
+      }
+    }
+    
+    
+  }
+}
 
 
 
